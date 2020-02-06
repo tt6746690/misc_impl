b24c625bcc58a70898ec0ff61bb8a2aebdc2ee3a
Mark Wang <peiqi1122@gmail.com>
2020-02-05 22:55:27 -0500

    add

---
+++ eigenfaces/README.md
@@ -0,0 +1,40 @@
+
+
++ implementation for 
+    + [A Tutorial on Principal Component Analysis](https://www.cs.cmu.edu/~elaw/papers/pca.pdf)
+    + [Eigenfaces for Recognition](https://www.mitpressjournals.org/doi/10.1162/jocn.1991.3.1.71)
+
+#### Original Data
+
+<p align="center">
+    <img src="assets/X.png">
+</p>
+
+#### Eigenfaces
+
+<p align="center">
+    <img src="assets/eigenfaces.png">
+</p>
+
+#### Reconstructed data using first 5, 50, 100, 500
+
+<p align="center">
+    <img src="assets/Yhat_npc=5.png">
+</p>
+
+<p align="center">
+    <img src="assets/Yhat_npc=50.png">
+</p>
+
+<p align="center">
+    <img src="assets/Yhat_npc=100.png">
+</p>
+
+<p align="center">
+    <img src="assets/Yhat_npc=500.png">
+</p>
+
+
+## references
+
++ [image dataset](https://scikit-learn.org/stable/auto_examples/applications/plot_face_recognition.html)
---
+++ eigenfaces/eigenfaces.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+def pca_with_eig(X, n_components=None):
+    N = X.shape[0]
+    X = X - np.mean(X,axis=0)
+    C = 1/(N-1)*X.T@X
+    w, P = np.linalg.eig(C)
+    I = np.argsort(-w)
+    P = P[:,I]
+    if n_components:
+        assert(n_components <= P.shape[1])
+        P = P[:,:n_components]
+    Y = X@P
+    return Y, P
+
+def pca_with_svd(X, n_components=None):
+    N = X.shape[0]
+    X = X - np.mean(X,axis=0)
+    W = X/(N-1)
+    _, _, VT = np.linalg.svd(W)
+    P = VT.T
+    if n_components:
+        assert(n_components <= P.shape[1])
+        P = P[:,:n_components]
+    Y = X@P
+    return Y, P
+
+
+if __name__ == '__main__':
+
+    import os
+    os.makedirs('./assets', exist_ok=True)
+
+    import math
+    import matplotlib.pyplot as plt
+    import torch
+    import torchvision
+    # labelled faces in the wild: http://vis-www.cs.umass.edu/lfw/
+    from sklearn.datasets import fetch_lfw_people
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_pc", type=int, dest='n_pc', default=150)
+    parser.add_argument("--n_save", type=int, dest='n_save', default=40)
+    args = parser.parse_args() 
+    globals().update(vars(args))
+
+    n_save = min([n_save, n_pc])
+
+    lfw_people = fetch_lfw_people(
+        data_home='./data',
+        min_faces_per_person=70,
+        resize=0.4)
+    n_samples, h, w = lfw_people.images.shape
+
+    X = lfw_people.data
+    Xmean = np.mean(X, axis=0)
+    Y, P = pca_with_svd(X, n_pc)
+
+    eigenfaces = np.moveaxis(P, 0, 1).reshape((n_pc, 1, h, w))
+    torchvision.utils.save_image(
+        torch.tensor(eigenfaces)[:n_save],
+        './assets/eigenfaces.png',
+        padding=1,
+        normalize=True)
+
+    torchvision.utils.save_image(
+        torch.tensor(X[:n_save,:].reshape(-1,1,h,w)),
+        f'./assets/X.png',
+        padding=1,
+        normalize=True)
+
+
+    for n_pc in [5, 50, 100, 500]:
+        Y, P = pca_with_svd(X, n_pc)
+        recon = Y[:n_save,:] @ P.T + Xmean
+        torchvision.utils.save_image(
+            torch.tensor(recon.reshape((-1, 1, h, w))),
+            f'./assets/Yhat_npc={n_pc}.png',
+            padding=1,
+            normalize=True)
