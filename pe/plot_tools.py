f989d15880748f74f7403478634fe6bd4f4129bf
Mark <peiqi1122@gmail.com>
2020-02-21 17:40:17 -0500

    add

---
+++ pe/plot_tools.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+import torchvision
+
+def plot_im(ims, n_row=8, plot_title=None, im_path=None, fig_size=None):
+    if isinstance(ims, torch.Tensor):
+        ims = ims.cpu().detach()
+        ims = ims.float()
+    ims = torchvision.utils.make_grid(
+        ims, padding=1, normalize=True, nrow=n_row)
+    ims = np.transpose(ims, (1,2,0))
+    plt.figure(figsize=fig_size)
+    plt.imshow(ims)
+    plt.axis('off')
+    if plot_title:
+        plt.title(plot_title)
+    if im_path:
+        plt.savefig(im_path)
+        plt.clf()
+        plt.close('all')
+    else:
+        plt.show()
