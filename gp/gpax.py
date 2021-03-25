453cb6e96bcee27bd4de8b04a003fa4d0d21b256
Mark Wang <peiqi1122@gmail.com>
2021-03-25 16:02:58 -0400

    add

--- gp/gpax.py
+++ gp/gpax.py
@@ -19,10 +19,10 @@ class CovSE(nn.Module):
 
     def setup(self):
         self.transform = BijectiveSoftplus()
-        init_fn = lambda k, s: self.transform.reverse(np.array([1.]))
-        self.ℓ  = self.transform.forward(self.param('ℓ',  init_fn, (1,)))
+        def init_fn(k, s): return self.transform.reverse(np.array([1.]))
+        self.ℓ = self.transform.forward(self.param('ℓ',  init_fn, (1,)))
         self.σ2 = self.transform.forward(self.param('σ2', init_fn, (1,)))
-        
+
     def scale(self, X):
         return X/self.ℓ if X is not None else X
 
@@ -36,28 +36,29 @@ class CovSE(nn.Module):
 
 
 class LikNormal(nn.Module):
-    
+
     def setup(self):
         transform = BijectiveSoftplus()
-        init_fn = lambda k, s: transform.reverse(np.array([1.]))
+        def init_fn(k, s): return transform.reverse(np.array([1.]))
         self.σ2 = transform.forward(self.param('σ2', init_fn, (1,)))
-        
+
     def variational_expectation(self, y, μf, σ2f):
         """Computes E[log(p(y|f))] 
                 where f ~ N(μ, diag[v]) and y = \prod_i p(yi|fi)
-                      
+
         E[log(p(y|f))] = Σᵢ E[ -.5log(2πσ2) - (.5/σ2) (yᵢ^2 - 2yᵢfᵢ + fᵢ^2) ]
-                       = Σᵢ -.5log(2πσ2) - (.5/σ2) ((yᵢ-fᵢ)^2 + vᵢ)   by fᵢ^2 = vᵢ + μ^2
+                       = Σᵢ -.5log(2πσ2) - (.5/σ2) ((yᵢ-μᵢ)^2 + vᵢ)   by E[fᵢ]^2 = μᵢ^2 + vᵢ
         """
         return np.sum(-.5*np.log(2*np.pi*σ2) - (.5/σ2)*(np.square((y-μf)) + σ2f))
 
+
 class GPR(nn.Module):
     data: Tuple[np.ndarray, np.ndarray]
-        
+
     def setup(self):
         self.k = CovSE()
         self.lik = LikNormal()
-        
+
     def get_init_params(self, key):
         params = self.init(key, np.ones((1, self.data[0].shape[-1])),
                            method=self.pred_f)
@@ -67,23 +68,23 @@ class GPR(nn.Module):
         X, y = self.data
         k = self.k
         n = len(X)
-        
+
         K = k(X) + self.lik.σ2*np.eye(n)
         L = linalg.cholesky(K)
         α = cho_solve((L, True), y)
 
-        mll_mahan  = -(1/2)*np.sum(y*α)
-        mll_lgdet   = - np.sum(np.log(np.diag(L)))
+        mll_mahan = -(1/2)*np.sum(y*α)
+        mll_lgdet = - np.sum(np.log(np.diag(L)))
         mll_const = - (n/2)*np.log(2*np.pi)
         mll = mll_mahan + mll_lgdet + mll_const
 
         return mll
-    
+
     def pred_f(self, Xs):
         X, y = self.data
         k = self.k
         n = len(X)
-        
+
         K = k(X) + self.lik.σ2*np.eye(n)
         Ks = k(X, Xs)
         Kss = k(Xs, Xs)
@@ -92,9 +93,9 @@ class GPR(nn.Module):
         μ = Ks.T@α
         v = solve_triangular(L, Ks, lower=True)
         Σ = Kss - v.T@v
-        
+
         return μ, Σ
-    
+
     def pred_y(self, Xs):
         μf, Σf = self.pred_f(Xs)
         ns = len(Σf)
@@ -110,63 +111,63 @@ class GPRFITC(nn.Module):
         self.k = CovSE()
         self.lik = LikNormal()
         X, y = self.data
-        self.Xu = self.param('Xu', lambda k,s : X[:self.n_inducing],
+        self.Xu = self.param('Xu', lambda k, s: X[:self.n_inducing],
                              (self.n_inducing, X.shape[-1]))
 
     def get_init_params(self, key):
         params = self.init(key, np.ones((1, self.data[0].shape[-1])),
                            method=self.pred_f)
         return params
-    
+
     def precompute(self):
         X, y = self.data
         k = self.k
         Xu = self.Xu
         n, m = len(X), self.n_inducing
-        
+
         Kdiag = k(X, full_cov=True)
         Kuu = k(Xu)
         Kuf = k(Xu, X)
         Luu = cholesky_jitter(Kuu, jitter=1e-4)
-        
+
         V = solve_triangular(Luu, Kuf, lower=True)
         Qffdiag = np.sum(np.square(V), axis=0)
         Λ = Kdiag - Qffdiag + self.lik.σ2
-        Λ = Λ.reshape(-1,1)
-        
+        Λ = Λ.reshape(-1, 1)
+
         B = np.eye(m) + (V/Λ.T)@V.T
         LB = cholesky_jitter(B, jitter=1e-5)
         γ = solve_triangular(LB, V@(y/Λ), lower=True)
 
         return Luu, Λ, LB, γ
-    
+
     def mll(self):
         X, y = self.data
         n = len(X)
         Luu, Λ, LB, γ = self.precompute()
-        
-        mll_mahan  = -.5*( np.sum((y/Λ)*y) - np.sum(np.square(γ)) )
-        mll_lgdet   = -np.sum(np.log(np.diag(LB)))-.5*np.sum(np.log(Λ))
+
+        mll_mahan = -.5*(np.sum((y/Λ)*y) - np.sum(np.square(γ)))
+        mll_lgdet = -np.sum(np.log(np.diag(LB)))-.5*np.sum(np.log(Λ))
         mll_const = -(n/2)*np.log(2*np.pi)
         mll = mll_mahan + mll_lgdet + mll_const
 
         return mll
-    
+
     def pred_f(self, Xs):
         X, y = self.data
         k = self.k
         Xu = self.Xu
         n = len(X)
         Luu, Λ, LB, γ = self.precompute()
-        
+
         Kss = k(Xs)
         Kus = k(Xu, Xs)
         ω = solve_triangular(Luu, Kus, lower=True)
         ν = solve_triangular(LB, ω, lower=True)
-        
+
         μ = ω.T@solve_triangular(LB.T, γ, lower=False)
         Σ = Kss - ω.T@ω + ν.T@ν
-        
+
         return μ, Σ
 
     def pred_y(self, Xs):
@@ -184,63 +185,64 @@ class VFE(nn.Module):
         self.k = CovSE()
         self.lik = LikNormal()
         X, y = self.data
-        self.Xu = self.param('Xu', lambda k,s : X[:self.n_inducing],
+        self.Xu = self.param('Xu', lambda k, s: X[:self.n_inducing],
                              (self.n_inducing, X.shape[-1]))
 
     def get_init_params(self, key):
         params = self.init(key, np.ones((1, self.data[0].shape[-1])),
                            method=self.pred_f)
         return params
-    
+
     def precompute(self):
         X, y = self.data
         k = self.k
         Xu = self.Xu
         n, m = len(X), self.n_inducing
-        
+
         Kdiag = k(X, full_cov=True)
         Kuu = k(Xu)
         Kuf = k(Xu, X)
         Luu = cholesky_jitter(Kuu, jitter=1e-4)
-        
+
         V = solve_triangular(Luu, Kuf, lower=True)
         Qffdiag = np.sum(np.square(V), axis=0)
         Λ = Kdiag - Qffdiag + self.lik.σ2
-        Λ = Λ.reshape(-1,1)
-        
+        Λ = Λ.reshape(-1, 1)
+
         B = np.eye(m) + (V/Λ.T)@V.T
         LB = cholesky_jitter(B, jitter=1e-5)
         γ = solve_triangular(LB, V@(y/Λ), lower=True)
 
         return Kdiag, Luu, V, Λ, LB, γ
-    
+
     def mll(self):
         X, y = self.data
         n = len(X)
         Kdiag, Luu, V, Λ, LB, γ = self.precompute()
-        
-        elbo_mahan  = -.5*( np.sum((y/Λ)*y) - np.sum(np.square(γ)) )
-        elbo_lgdet   = -np.sum(np.log(np.diag(LB)))-.5*np.sum(np.log(Λ))
+
+        elbo_mahan = -.5*(np.sum((y/Λ)*y) - np.sum(np.square(γ)))
+        elbo_lgdet = -np.sum(np.log(np.diag(LB)))-.5*np.sum(np.log(Λ))
         elbo_const = -(n/2)*np.log(2*np.pi)
-        elbo_trace  = -(1/2/self.lik.σ2[0])*( np.sum(Kdiag) - np.sum(np.square(V)) )
-        
+        elbo_trace = -(1/2/self.lik.σ2[0]) * \
+            (np.sum(Kdiag) - np.sum(np.square(V)))
+
         elbo = elbo_mahan + elbo_lgdet + elbo_const + elbo_trace
         return elbo
-    
+
     def pred_f(self, Xs):
         X, y = self.data
         k = self.k
         Xu = self.Xu
         Kdiag, Luu, V, Λ, LB, γ = self.precompute()
-        
+
         Kss = k(Xs)
         Kus = k(Xu, Xs)
         ω = solve_triangular(Luu, Kus, lower=True)
         ν = solve_triangular(LB, ω, lower=True)
-        
+
         μ = ω.T@solve_triangular(LB.T, γ, lower=False)
         Σ = Kss - ω.T@ω + ν.T@ν
-        
+
         return μ, Σ
 
     def pred_y(self, Xs):
@@ -250,13 +252,75 @@ class VFE(nn.Module):
         return μy, Σy
 
 
+class SVGP(nn.Module):
+    data: Tuple[np.ndarray, np.ndarray]
+    n_inducing: int
+
+    def setup(self):
+        self.k = CovSE()
+        self.lik = LikNormal()
+        X, y = self.data
+        self.Xu = self.param('Xu', lambda k, s: X[:self.n_inducing],
+                             (self.n_inducing, X.shape[-1]))
+        self.μq = self.param('μq', jax.nn.initializers.zeros,
+                             (n_inducing, 1))
+        Luu = cholesky_jitter(self.k(self.Xu), 1e-4)
+        def init_fn_Lq(k, s): return BijectiveFillTril.reverse(Luu)
+        self.Lq = BijectiveFillTril.forward(
+            self.param('Lq', init_fn_Lq,
+                       (BijectiveFillTril.reverse_shape(n_inducing), 1)))
+
+    def get_init_params(self, key):
+        Xs = np.ones((1, self.data[0].shape[-1]))
+        ys = np.ones((1, self.data[1].shape[-1]))
+        params = self.init(key, method=self.mll)
+        return params
+
+    def mll(self):
+        X, y = self.data
+        k = self.k
+        m = self.n_inducing
+        Xu, μq, Lq = self.Xu, self.μq, self.Lq
+
+        Kss = k(X, full_cov=False)
+        Kus = k(Xu, X)
+        Kuu = k(Xu, Xu)
+        Luu = cholesky_jitter(Kuu, jitter=1e-6)
+
+        μqf, σ2qf = vgp_qf_tril(Kss, Kus, Luu, μq, Lq, full_cov=False)
+        elbo_lik = self.lik.variational_expectation(y, μqf, σ2qf)
+        elbo_kl = -kl_mvn_tril_zero_mean_prior(μq, Lq, Luu)
+        elbo = elbo_lik + elbo_kl
+
+        return elbo
+
+    def pred_f(self, Xs, full_cov=False):
+        k = self.k
+        m = self.n_inducing
+        Xu, μq, Lq = self.Xu, self.μq, self.Lq
+
+        Kss = k(Xs, full_cov=full_cov)
+        Kus = k(Xu, Xs)
+        Kuu = k(Xu, Xu)
+        Luu = cholesky_jitter(Kuu, jitter=1e-6)
+
+        μf, Σf = vgp_qf_tril(Kss, Kus, Luu, μq, Lq, full_cov=full_cov)
+        return μf, Σf
+
+    def pred_y(self, Xs):
+        μf, Σf = self.pred_f(Xs)
+        ns = len(Σf)
+        μy, Σy = μf, Σf + self.lik.σ2*np.diag(np.ones((ns,)))
+        return μy, Σy
+
+
 class BijectiveExp(object):
-    
+
     @staticmethod
     def forward(x):
         """ x -> exp(x) \in \R+ """
         return np.exp(x)
-    
+
     @staticmethod
     def reverse(y):
         return np.log(y)
@@ -279,7 +343,7 @@ class BijectiveSoftplus(object):
     @staticmethod
     def forward(x):
         return jax.nn.softplus(x)
-    
+
     @staticmethod
     def reverse(y):
         return softplus_inv(y)
@@ -290,7 +354,7 @@ class BijectiveFillTril(object):
             v (n,) -> L (m,m)
                 where `m = (-1+sqrt(1+8*n))/2`
                       `n = m*(m+1)/2`.`
-                      
+
         ```
             v = np.arange(6)
             L = FillTril.forward(v)
@@ -309,23 +373,23 @@ class BijectiveFillTril(object):
     @staticmethod
     def forward_shape(n):
         return int((-1+math.sqrt(1+8*n))/2)
-    
+
     @staticmethod
     def reverse_shape(m):
         return int(m*(m+1)/2)
-    
+
     @staticmethod
     def forward(v):
         m = BijectiveFillTril.forward_shape(v.size)
-        L = np.zeros((m,m))
+        L = np.zeros((m, m))
         L = jax.ops.index_update(L, np.tril_indices(m), v.squeeze())
         return L
-    
+
     @staticmethod
     def reverse(L):
         m = len(L)
         v = L[np.tril_indices(m)]
-        v = v.reshape(-1,1)
+        v = v.reshape(-1, 1)
         return v
 
 
@@ -334,26 +398,27 @@ class BijectiveSoftplusFillTril(object):
     @staticmethod
     def forward_shape(n):
         return int((-1+math.sqrt(1+8*n))/2)
-    
+
     @staticmethod
     def reverse_shape(m):
         return int(m*(m+1)/2)
-    
+
     @staticmethod
     def forward(v):
         m = BijectiveSoftplusFillTril.forward_shape(v.size)
-        L = np.zeros((m,m))
+        L = np.zeros((m, m))
         L = jax.ops.index_update(L, np.tril_indices(m, k=-1), v[:-m].squeeze())
-        L = jax.ops.index_update(L, np.diag_indices(m), jax.nn.softplus(v[-m:].squeeze()))
+        L = jax.ops.index_update(L, np.diag_indices(
+            m), jax.nn.softplus(v[-m:].squeeze()))
         return L
-    
+
     @staticmethod
     def reverse(L):
         m = len(L)
         v1 = L[np.tril_indices(m, k=-1)]
         v2 = softplus_inv(L[np.diag_indices(m)])
         v = np.concatenate((v1, v2), axis=-1)
-        v = v.reshape(-1,1)
+        v = v.reshape(-1, 1)
         return v
 
 
@@ -402,11 +467,11 @@ def vgp_qf_unstable(Kff, Kuf, Kuu, μq, Σq, full_cov=False):
 def vgp_qf_tril(Kff, Kuf, Luu, μq, Lq, full_cov=False):
     """q(f) = \int p(f|u)q(u) du
             = N(Kfu Kuu^-1 μq, Kff - Qff + Kfu Kuu^-1 Σq Kuu^-1 Kuf)
-            
-        where q(u)   ~ N(μu, Σu) w/  Σu := Lu@Lu.T
+
+        where q(u)   ~ N(μq, Σq) w/  Σq := Lq@Lq.T
               p(u)   ~ N(0, Kuu) w/ Kuu := Luu@Luu.T
               p(f|u) ~ N(0, Kfu Kuu^-1 u, Kff - Qff)
-              
+
         when `full_cov=True`
             assume `Kff` is also the diagonal
     """
@@ -424,11 +489,11 @@ def vgp_qf_tril(Kff, Kuf, Luu, μq, Lq, full_cov=False):
 
 
 def rand_μΣ(key, m):
-    μ = random.normal(key, (m,1))
-    Σ = random.normal(key, (m,m))
+    μ = random.normal(key, (m, 1))
+    Σ = random.normal(key, (m, m))
     Σ = jax.ops.index_update(Σ, np.tril_indices(m), 0)
     Σ = Σ@Σ.T+0.1*np.eye(m)
-    return μ,Σ
+    return μ, Σ
 
 
 def kl_mvn(μ0, Σ0, μ1, Σ1):
@@ -438,13 +503,13 @@ def kl_mvn(μ0, Σ0, μ1, Σ1):
     kl_mahan = np.sum((μ1-μ0).T@linalg.solve(Σ1, (μ1-μ0)))
     kl_const = -n
     kl_lgdet = np.log(linalg.det(Σ1)) - np.log(linalg.det(Σ0))
-    kl = .5*( kl_trace + kl_mahan + kl_const + kl_lgdet )
+    kl = .5*(kl_trace + kl_mahan + kl_const + kl_lgdet)
     return kl
 
 
 def kl_mvn_tril(μ0, L0, μ1, L1):
     """KL(q||p) where q~N(μ0,L0@L0.T), p~N(μ1,L1@L1.T) 
-    
+
         ```
             m = 50
             μ0,Σ0 = rand_μΣ(jax.random.PRNGKey(0), m)
@@ -464,8 +529,9 @@ def kl_mvn_tril(μ0, L0, μ1, L1):
     kl_mahan = np.sum(np.square(β))
     kl_const = -n
     kl_lgdet = 2*(np.sum(np.log(np.diag(L1))) - np.sum(np.log(np.diag(L0))))
-    kl = .5*( kl_trace + kl_mahan + kl_const + kl_lgdet )
+    kl = .5*(kl_trace + kl_mahan + kl_const + kl_lgdet)
     return kl
+
 
 def kl_mvn_tril_zero_mean_prior(μ0, L0, L1):
     """KL(q||p) where q~N(μ0,L0@L0.T), p~N(0,L1@L1.T) """
@@ -476,8 +542,9 @@ def kl_mvn_tril_zero_mean_prior(μ0, L0, L1):
     kl_mahan = np.sum(np.square(β))
     kl_const = -n
     kl_lgdet = 2*(np.sum(np.log(np.diag(L1))) - np.sum(np.log(np.diag(L0))))
-    kl = .5*( kl_trace + kl_mahan + kl_const + kl_lgdet )
+    kl = .5*(kl_trace + kl_mahan + kl_const + kl_lgdet)
     return kl
+
 
 def cholesky_jitter(K, jitter=1e-6):
     L = linalg.cholesky(K+jitter*np.eye(len(K)))
@@ -490,28 +557,41 @@ def randsub_init_fn(key, shape, dtype=np.float32, X=None):
     return X[idx]
 
 
-proc_leaf_scalar_exponentiate = lambda k, v: \
-    (k.split('log')[1], np.exp(v[0])) if (k.startswith('log') and v.size==1) else (k, v)
-proc_leaf_vector_exponentiate = lambda k, v: \
-    (k.split('log')[1], np.exp(v)) if (k.startswith('log') and v.size>1) else (k, v)
+def proc_leaf_scalar_exponentiate(k, v): return \
+    (k.split('log')[1], np.exp(v[0])) if (
+        k.startswith('log') and v.size == 1) else (k, v)
+
+
+def proc_leaf_vector_exponentiate(k, v): return \
+    (k.split('log')[1], np.exp(v)) if (
+        k.startswith('log') and v.size > 1) else (k, v)
+
+
 PROC_LEAF_VECTOR_LENGTH_LIMIT = 5
-proc_leaf_vector_firstn = lambda k, v: \
+
+
+def proc_leaf_vector_firstn(k, v): return \
     (f'{k}[:{PROC_LEAF_VECTOR_LENGTH_LIMIT}]', v[:PROC_LEAF_VECTOR_LENGTH_LIMIT]) \
-        if isinstance(v, np.ndarray) and v.size>1 else (k, v)
-proc_leaf_vector_squeeze = lambda k, v: \
+    if isinstance(v, np.ndarray) and v.size > 1 else (k, v)
+
+
+def proc_leaf_vector_squeeze(k, v): return \
     (k, v.squeeze()) if isinstance(v, np.ndarray) else (k, v)
+
+
 prof_leaf_fns = [proc_leaf_scalar_exponentiate,
                  proc_leaf_vector_exponentiate,
                  proc_leaf_vector_firstn,
                  proc_leaf_vector_squeeze]
 
+
 def log_func_simple(i, f, params, everyn=10):
-    if i%everyn==0:
+    if i % everyn == 0:
         print(f'[{i:3}]\tLoss={f(params):.3f}')
 
 
 def log_func_default(i, f, params, everyn=20):
-    if i%everyn == 0:
+    if i % everyn == 0:
         flattened = flax.traverse_util.flatten_dict(unfreeze(params['params']))
         S = []
         for k, v in flattened.items():
@@ -521,7 +601,7 @@ def log_func_default(i, f, params, everyn=20):
             k = list(k)
             k[-1] = lk
             k = '.'.join(k)
-            S.append(f'{k}={v:.3f}' if v.size==1 else f'{k}={v}')
+            S.append(f'{k}={v:.3f}' if v.size == 1 else f'{k}={v}')
 
         S = '\t'.join(S)
         print(f'[{i:3}]\tLoss={f(params):.3f}\t{S}')
@@ -543,7 +623,8 @@ def flax_run_optim(f, params, num_steps=10, log_func=None,
     for i in range(num_steps):
         fx, grad = fg_fn(opt.target)
         opt = opt.apply_gradient(grad)
-        if log_func is not None: log_func(i, f, opt.target)
+        if log_func is not None:
+            log_func(i, f, opt.target)
     return opt.target
 
 
